598dae1e5d81b3bbdff3c9ee0d85b9b5e91f4b6e
A0139923X\Chen Junsheng <E0003808@u.nus.edu>
2016-09-09 23:31:24 +0800

    Pass the test for Null name

--- src/seedu/addressbook/data/person/NameTest.java
+++ src/seedu/addressbook/data/person/NameTest.java
@@ -2,6 +2,7 @@ package seedu.addressbook.data.person;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import seedu.addressbook.data.exception.IllegalValueException;
